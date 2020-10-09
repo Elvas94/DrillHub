8ce4439458d122a17ee1da74d0921e0bd3abb8db
Elvas <elvas94@gmail.com>
2020-10-09 23:34:01 +0300

    Added dataAccess

---
+++ DrillHub.DataAccess/DrillHubContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DrillHub.DataAccess
+{
+    public class DrillHubContext : DbContext
+    {
+        public DbSet<DrillHub.Model.Products.Product> Products { get; set; }
+        public DbSet<DrillHub.Model.Orders.Order> Orders { get; set; }
+        public DbSet<DrillHub.Model.Products.ProductOrder> ProductOrders { get; set; }
+        public DbSet<DrillHub.Model.Categories.Category> Categories { get; set; }
+        public DbSet<DrillHub.Model.Subcategories.Subcategory> Subcategories { get; set; }
+
+        public DrillHubContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ DrillHub.DataAccess/TemporaryDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace DrillHub.DataAccess
+{
+    public class TemporaryDbContextFactory: IDesignTimeDbContextFactory<DrillHubContext>
+    {
+        public DrillHubContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"))
+                .AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.Development.json"))
+                .Build();
+
+            return new DrillHubContext(new DbContextOptionsBuilder<DrillHubContext>()
+                .UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    builder =>
+                    {
+                        builder.CommandTimeout((int)TimeSpan.FromMinutes(10).TotalSeconds);
+                    })
+                .Options);
+        }
+    }
+}
