c5429ff7f1605565f72756ad31bdb4fb52310787
Elvas <elvas94@gmail.com>
2020-10-12 01:08:34 +0300

    Added consoleRunner

---
+++ DrillHub.ConsoleRunner/DependencyInjector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DrillHub.ConsoleRunner
+{
+    public  class DependencyInjector
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private static readonly object Lock = new object();
+        private static DependencyInjector _instance;
+
+        public static DependencyInjector Instance
+        {
+            get
+            {
+                lock (Lock)
+                {
+                    return _instance ??= new DependencyInjector();
+                }
+            }
+        }
+
+        private DependencyInjector()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["DrillHubContext"].ConnectionString;
+
+            IServiceCollection services = new ServiceCollection();
+            services.AddDbContext<DataAccess.DrillHubContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            Model.ServiceLocator.RegisterServices(services);
+            Repositories.ServiceLocator.RegisterServices(services);
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        public T Resolve<T>() => _serviceProvider.GetRequiredService<T>();
+    }
+}
---
+++ DrillHub.ConsoleRunner/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using DrillHub.Model;
+
+namespace DrillHub.ConsoleRunner
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var categoryNamePrefix = new string(' ', 4);
+            //var subCategoryNamePrefix = new string(' ', 8);
+
+            var folderPath = @"D:\Projects\DrillHub\Docs";
+            var fileName = "Копия Prays-list_25_09_2020.xlsx";
+            var pathFile = Path.Combine(folderPath, fileName);
+
+            var service = DependencyInjector.Instance.Resolve<ExcelService>();
+
+            using var stream = File.OpenRead(pathFile);
+            service.UpdateDataBaseFromStream(stream, categoryNamePrefix);
+        }
+    }
+}
