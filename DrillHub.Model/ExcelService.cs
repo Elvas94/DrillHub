ba33500b6450d1de6ee18a874b34aeccf47b4d7c
Elvas <elvas94@gmail.com>
2020-10-13 01:12:54 +0300

    Change ExcelService

--- DrillHub.Model/ExcelService.cs
+++ DrillHub.Model/ExcelService.cs
@@ -1,89 +1,209 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using DrillHub.ConsoleRunner;
+using DrillHub.Infrastructure;
 using DrillHub.Model.Categories;
 using DrillHub.Model.Products;
-using Spire.Xls;
 using DrillHub.Model.SubCategories;
+using NPOI.SS.UserModel;
 
 namespace DrillHub.Model
 {
     public class ExcelService
     {
-        public async Task UpdateDataBaseFromStream(
-            FileStream stream,
-            string categoryNamePrefix)
+        private readonly IRepository<Category, int> _categoryRepository;
+        private readonly IRepository<SubCategory, int> _subCategoryRepository;
+        private readonly IRepository<Product, int> _productRepository;
+
+        public ExcelService(
+            IRepository<Category, int> categoryRepository,
+            IRepository<SubCategory, int> subCategoryRepository,
+            IRepository<Product, int> productRepository)
         {
-            var categories = await ParseExcelAsync(stream, categoryNamePrefix);
+            _categoryRepository = categoryRepository;
+            _subCategoryRepository = subCategoryRepository;
+            _productRepository = productRepository;
         }
 
-        public async Task<List<Category>> ParseExcelAsync(FileStream stream, string categoryNamePrefix)
+        public void InsertCategoriesWithSubCategoriesWithProduct(List<Category> categories)
         {
-            var countThread = Environment.ProcessorCount / 2;
+            _categoryRepository.InsertRange(categories);
+            _categoryRepository.SaveChanges();
+        }
 
-            var workbook = new Workbook();
+        public void InsertOrUpdateCategoriesWithSubCategoriesWithProduct(List<Category> categories)
+        {
+            //var dbCategories = _categoryRepository
+            //    .Query(c=>c.SubCategories).ToList();
+            
+            //foreach (var category in categories)
+            //{
+            //    var dbCategory = dbCategories.FirstOrDefault(item => item.Name == category.Name);
+            //    if (dbCategory == null)
+            //    {
+            //        _categoryRepository.Insert(category);
+            //        _categoryRepository.SaveChanges();
+            //    }
 
-            workbook.LoadFromStream(stream);
+            //    foreach (var subCategory in category.SubCategories)
+            //    {
+            //        var dbSubCategory = dbCategories.SelectMany(item => item.SubCategories)
+            //            .FirstOrDefault(item => item.Name == subCategory.Name);
 
-            if (!workbook.FileName.EndsWith(".xlsx"))
-            {
-                throw new ApplicationException(" Формат файла должен быть .xlsx");
-            }
+            //        if (dbSubCategory == null)
+            //        {
+            //            subCategory.CategoryId = category.Id;
+            //            _subCategoryRepository.Insert(subCategory);
+            //            _subCategoryRepository.SaveChanges();
+            //        }
 
-            var ranges = GetRanges(workbook, countThread, categoryNamePrefix);
+            //        foreach (var product in subCategory.Products)
+            //        {
+            //            var dbProduct = dbCategories
+            //                .SelectMany(item => item.SubCategories)
+            //                .SelectMany(item => item.Products)
+            //                .FirstOrDefault(item => item.VendorCode == product.VendorCode || item.OriginalName == product.OriginalName);
 
-            var tasks = ranges.Select(range =>
-               Task.Run(() =>
-                  GetCategoriesWithSubCategoriesWithProduct(
-                   workbook,
-                   range,
-                   categoryNamePrefix)))
-               .ToList();
+            //            if (dbProduct == null)
+            //            {
+            //                _productRepository.Insert(product);
+            //                _subCategoryRepository.SaveChanges();
+            //            }
+            //            else
+            //            {
+            //                dbProduct.SubCategoryId = subCategory.Id;
+            //                dbProduct.OriginalName = product.OriginalName;
+            //                dbProduct.DisplayName = product.DisplayName;
+            //                dbProduct.Price = product.Price;
+            //                dbProduct.UnitType = product.UnitType;
+            //                dbProduct.QuantityInStock = product.QuantityInStock;
+            //            }
+            //        }
+            //    }
+            //}
+            //_categoryRepository.InsertRange(categories);
+            //_categoryRepository.SaveChanges();
+        }
 
-            var result = (await Task.WhenAll(tasks)).SelectMany(item => item).ToList();
+        public void UpdateDataBaseFromStream(FileStream stream, string categoryNamePrefix)
+        {
+            var categories = ParseExcel(stream, categoryNamePrefix);
+            InsertCategoriesWithSubCategoriesWithProduct(categories);
+        }
 
-            //var categoriesCount = result.Count();
-            //var subCategoriesCount = result.SelectMany(item => item.SubCategories).Count(item => !item.IsCategory);
-            //var product = result.SelectMany(item => item.SubCategories).SelectMany(item => item.Products).Count();
+        public List<Category> ParseExcel(FileStream stream, string categoryNamePrefix)
+        {
+            var workbook = WorkbookFactory.Create(stream);
+
+            var sheet = workbook.GetSheetAt(0);
+
+            var ranges = GetRanges(sheet, 1, categoryNamePrefix);
+
+            var result =
+                GetCategoriesWithSubCategoriesWithProduct(sheet, ranges.First(), categoryNamePrefix);
+
+            var categoriesCount = result.Count();
+            var subCategoriesCount = result.SelectMany(item => item.SubCategories).Count(item => !item.IsCategory);
+            var product = result.SelectMany(item => item.SubCategories).SelectMany(item => item.Products).Count();
 
             return result;
         }
 
-        public async Task<List<Range>> GetRangesAsync(Workbook workbook, int countThread, string categoryNamePrefix)
+        public List<Category> GetCategoriesWithSubCategoriesWithProduct(
+           ISheet sheet,
+           Range range,
+           string categoryNamePrefix)
         {
-            var sheet = workbook.Worksheets[0];
-            var length = sheet.Rows.Length;
-            var cutoff = length / countThread;
+            var categories = new List<Category>();
+            var currentCategory = new Category();
+            var currentSubCategory = new SubCategory();
 
-            var tasks = Enumerable.Range(0, countThread).Select((item, index) =>
-                  Task.Run(() =>
-                     GetIndexStartByRange(
-                        sheet,
-                        new Range
+            for (var index = range.StartIndex; index <= range.EndIndex; index++)
+            {
+                //Console.WriteLine($"Process {i}/{range.EndIndex}");
+                var vendorCodeCell = sheet.GetRow(index).GetCell(1);
+
+                var isProduct = vendorCodeCell.CellType == CellType.Numeric;
+                var vendorCode = isProduct ? vendorCodeCell.NumericCellValue.ToString(CultureInfo.InvariantCulture) : "";
+                var name = sheet.GetRow(index).GetCell(2).StringCellValue;
+
+                var countSpaceFromStartInName = name.Split(' ').Count(string.IsNullOrWhiteSpace);
+
+                if (string.IsNullOrWhiteSpace(vendorCode))
+                {
+                    if (countSpaceFromStartInName == categoryNamePrefix.Length)
+                    {
+                        var category = new Category
                         {
-                            StartIndex = index * cutoff,
-                            EndIndex = (index + 1) * cutoff
-                        },
-                        categoryNamePrefix)))
-               .ToList();
+                            Name = name.Trim(),
+                            SubCategories = new List<SubCategory>()
+                        };
 
-            var startIndexes = (await Task.WhenAll(tasks)).Where(item => item != -1).ToList();
+                        currentCategory = category;
+                        categories.Add(category);
 
-            return startIndexes.Select((item, index) =>
-               new Range
-               {
-                   StartIndex = item,
-                   EndIndex = index == startIndexes.Count - 1 ? length : startIndexes[index + 1] - 1
-               }).ToList();
+                        currentSubCategory = null;
+
+                        continue;
+                    }
+
+                    if (countSpaceFromStartInName > categoryNamePrefix.Length)
+                    {
+                        var subCategory = new SubCategory
+                        {
+                            Name = name.Trim(),
+                            Products = new List<Product>(),
+                            IsCategory = false
+                        };
+
+                        currentSubCategory = subCategory;
+
+                        currentCategory.SubCategories.Add(subCategory);
+                    }
+                }
+
+                else
+                {
+                    var quantityInStock = (int)sheet.GetRow(index).GetCell(3).NumericCellValue;
+                    var price = (decimal)sheet.GetRow(index).GetCell(4).NumericCellValue;
+
+                    var test = sheet.GetRow(index).GetCell(5);
+                    var unitType = sheet.GetRow(index).GetCell(5).StringCellValue.Contains("шт") ? UnitType.Single : UnitType.Collection;
+
+                    var trimName = name.Trim();
+
+                    var product = new Product
+                    {
+                        DisplayName = trimName,
+                        OriginalName = trimName,
+                        VendorCode = vendorCode.Trim(),
+                        QuantityInStock = quantityInStock,
+                        Price = price,
+                        UnitType = unitType
+                    };
+
+                    if (currentSubCategory == null)
+                    {
+                        currentSubCategory = new SubCategory
+                        {
+                            Name = currentCategory.Name.Trim(),
+                            IsCategory = true,
+                            Products = new List<Product>()
+                        };
+                        currentCategory.SubCategories.Add(currentSubCategory);
+                    }
+
+                    currentSubCategory.Products.Add(product);
+                }
+            }
+            return categories;
         }
 
-        public List<Range> GetRanges(Workbook workbook, int countThread, string categoryNamePrefix)
+        public List<Range> GetRanges(ISheet sheet, int countThread, string categoryNamePrefix)
         {
-            var sheet = workbook.Worksheets[0];
-            var length = sheet.Rows.Length;
+            var length = sheet.LastRowNum;
             var cutoff = length / countThread;
 
             var startIndexes = new List<int>();
@@ -95,12 +215,15 @@ namespace DrillHub.Model
 
                 for (var index = currentCutoffValue; index < nextCutoffValue; index++)
                 {
-                    var vendorCode = sheet.Rows[index].Columns[1].Value;
-                    var name = sheet.Rows[index].Columns[2].Value;
+                    var vendorCodeCell = sheet.GetRow(index).GetCell(1);
+                    var name = sheet.GetRow(index).GetCell(2)?.StringCellValue;
+                    var countSpaceFromStartInName = name?.Split(' ').Count(string.IsNullOrWhiteSpace);
 
-                    var countSpaceFromStartInName = name.Split(' ').Count(string.IsNullOrWhiteSpace);
+                    var isCategory = vendorCodeCell?.CellType == CellType.Blank
+                                     && string.IsNullOrWhiteSpace(vendorCodeCell?.StringCellValue)
+                                     && countSpaceFromStartInName == categoryNamePrefix.Length;
 
-                    if (string.IsNullOrWhiteSpace(vendorCode) && countSpaceFromStartInName == categoryNamePrefix.Length)
+                    if (isCategory)
                     {
                         startIndexes.Add(index);
                         break;
@@ -114,109 +237,6 @@ namespace DrillHub.Model
                    StartIndex = item,
                    EndIndex = index == startIndexes.Count - 1 ? length : startIndexes[index + 1] - 1
                }).ToList();
-        }
-
-        public int GetIndexStartByRange(Worksheet sheet, Range range, string categoryNamePrefix)
-        {
-            for (var index = range.StartIndex; index < range.EndIndex; index++)
-            {
-                var vendorCode = sheet.Rows[index].Columns[1].Value;
-                var name = sheet.Rows[index].Columns[2].Value;
-
-                var countSpaceFromStartInName = name.Split(' ').Count(string.IsNullOrWhiteSpace);
-
-                if (string.IsNullOrWhiteSpace(vendorCode) && countSpaceFromStartInName == categoryNamePrefix.Length)
-                {
-                    return index;
-                }
-            }
-
-            return -1;
-        }
-
-        public List<Category> GetCategoriesWithSubCategoriesWithProduct(
-           Workbook workbook,
-           Range range,
-           string categoryNamePrefix)
-        {
-            var sheet = workbook.Worksheets[0];
-
-            var categories = new List<Category>();
-            var currentCategory = new Category();
-            var currentSubCategory = new Subcategory();
-
-            for (var i = range.StartIndex; i < range.EndIndex; i++)
-            {
-                //Console.WriteLine($"Process {i}/{range.EndIndex}");
-
-                var vendorCode = sheet.Rows[i].Columns[1].Value;
-                var name = sheet.Rows[i].Columns[2].Value;
-
-                var countSpaceFromStartInName = name.Split(' ').Count(string.IsNullOrWhiteSpace);
-
-                if (string.IsNullOrWhiteSpace(vendorCode))
-                {
-                    if (countSpaceFromStartInName == categoryNamePrefix.Length)
-                    {
-                        var category = new Category
-                        {
-                            Name = name.Trim(),
-                            Subcategories = new List<Subcategory>()
-                        };
-
-                        currentCategory = category;
-                        categories.Add(category);
-
-                        currentSubCategory = null;
-
-                        continue;
-                    }
-
-                    if (countSpaceFromStartInName > categoryNamePrefix.Length)
-                    {
-                        var subCategory = new Subcategory
-                        {
-                            Name = name.Trim(),
-                            Products = new List<Product>(),
-                            IsCategory = false
-                        };
-
-                        currentSubCategory = subCategory;
-
-                        currentCategory.Subcategories.Add(subCategory);
-                    }
-                }
-
-                else
-                {
-                    var quantityInStock = Int32.Parse(sheet.Rows[i].Columns[3].Value);
-                    var price = Decimal.Parse(sheet.Rows[i].Columns[4].Value.Replace(".руб", ""));
-                    var unitType = sheet.Rows[i].Columns[4].Value.Contains("шт") ? UnitType.Single : UnitType.Collection;
-
-                    var product = new Product
-                    {
-                        Name = name.Trim(),
-                        VendorCode = vendorCode.Trim(),
-                        QuantityInStock = quantityInStock,
-                        Price = price,
-                        UnitType = unitType
-                    };
-
-                    if (currentSubCategory == null)
-                    {
-                        currentSubCategory = new Subcategory
-                        {
-                            Name = currentCategory.Name.Trim(),
-                            IsCategory = true,
-                            Products = new List<Product>()
-                        };
-                        currentCategory.Subcategories.Add(currentSubCategory);
-                    }
-
-                    currentSubCategory.Products.Add(product);
-                }
-            }
-            return categories;
         }
     }
 }
