810e60d3a2d2d13993863dbfbfd932bd9dab85b1
Vasiliy Elfimov <Elvas7@mail.ru>
2021-05-04 23:55:31 +0300

    Описание методов контроллера для сваггера

--- DrillHub.WebApi/Controllers/Api/CategoryController.cs
+++ DrillHub.WebApi/Controllers/Api/CategoryController.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections.Generic;
 using DrillHub.Model.Categories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DrillHub.WebApi.Controllers.Api
 {
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
     public class CategoryController : ControllerBase
     {
         private readonly CategoryService _categoryService;
@@ -13,6 +17,14 @@ namespace DrillHub.WebApi.Controllers.Api
             _categoryService = categoryService;
         }
 
+        /// <summary>
+        /// Получить все категории с подкатегориями
+        /// </summary>
+        /// <remarks>
+        /// </remarks>
+        /// <returns>Категории с вложенными подкатегориями</returns>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Category>))]
+        [HttpGet]
         public List<Category> GetCategoriesWithSubCategories()
         {
             return _categoryService.GetCategoriesWithSubCategories();
